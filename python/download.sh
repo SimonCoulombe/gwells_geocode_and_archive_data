f5238a92a60c7ccde12033b12c78f9eb41c93340
Simon Coulombe <simoncoulombe@gmail.com>
2021-12-06 22:09:57 -0500

    remove empty lines

--- python/download.sh
+++ python/download.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 #set -euxo pipefail
-
 eval "$(conda shell.bash hook)"
-
 conda activate gwells_locationqa
-#which python
 python gwells_locationqa.py  download
--- python/geocode.sh
+++ python/geocode.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 #set -euxo pipefail
-
 eval "$(conda shell.bash hook)"
 conda activate gwells_locationqa
-#cp   /GWELLS_LocationQA/gwells_locationqa.py ~/gwells_locationqa.py
 python gwells_locationqa.py geocode  not_an_api_key
