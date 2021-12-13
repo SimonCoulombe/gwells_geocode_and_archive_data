f1f2f45adaf38bfa60ced0902c6cac7e1fa63b0d
Simon Coulombe <simoncoulombe@gmail.com>
2021-12-13 14:38:21 -0500

    more debug

--- R/clean_after_geocode.R
+++ R/clean_after_geocode.R
@@ -18,6 +18,8 @@ newly_geocoded <- read_csv("data/wells_geocoded.csv", col_types = col_types_geoc
   janitor::clean_names()
 
 if(nrow(newly_geocoded)> 0){
+  message("printing newly geocoded")
+  glimpse(newly_geocoded)
   message("Appending newly geocoded  wells:", nrow(newly_geocoded), " rows. well_tag_number=", paste(newly_geocoded$well_tag_number, collase = " "))
   dbAppendTable(con1, "wells_geocoded", newly_geocoded)
   
