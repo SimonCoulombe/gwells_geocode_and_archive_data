458f63e5130501d6d3306cef63fb89f9669d2052
Simon Coulombe <simoncoulombe@gmail.com>
2021-12-13 12:34:05 -0500

    test QA

--- R/clean_after_qa.R
+++ R/clean_after_qa.R
@@ -3,17 +3,27 @@ library(readr)
 library(janitor)
 
 source("R/col_types_wells.R")
+source("R/col_types_wells.R")
 
-#this script combines the old QA and the newly  QAed files
+library(RPostgres)
+con1 <- DBI::dbConnect(
+  RPostgres::Postgres(),
+  dbname = Sys.getenv("BCGOV_DB"),
+  host = Sys.getenv("BCGOV_HOST"),
+  user = Sys.getenv("BCGOV_USR"),
+  password=Sys.getenv("BCGOV_PWD")
+)
 
-old_qa  <- read_csv("github_data/gwells_locationqa.csv", col_types = col_types_qa) %>%
-  select(-one_of(c("Unnamed: 0")))
 
-new_qa <- read_csv("gwells_locationqa.csv", col_types = col_types_qa) %>%
-  select(-one_of(c("Unnamed: 0")))
+newly_qa <- read_csv("gwells_locationqa.csv", col_types = col_types_qa) %>%
+  select(-one_of(c("Unnamed: 0"))) %>% 
+  mutate(date_qa = as.Date(Sys.time() , tz = "America/Vancouver")) %>%
+  janitor::clean_names()
 
-all_qa <- bind_rows(old_qa, new_qa) %>%
-  arrange(well_tag_number)
 
-write_csv(all_qa, "data/gwells_locationqa.csv")
+if(nrow(newly_qa)> 0){
+  message("Appending newly qa  wells:", nrow(newly_qa), " rows. well_tag_number=", paste(newly_qa$well_tag_number, collase = " "))
+  dbAppendTable(con1, "wells_qa", newly_qa)
+  
+} else{message("No new qa wells to append.")}
 
