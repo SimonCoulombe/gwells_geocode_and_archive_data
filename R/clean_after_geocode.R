b3905906396876c1670916b9b92cb185a2c2c0df
Simon Coulombe <simoncoulombe@gmail.com>
2021-12-13 11:08:14 -0500

    geocode avec db?

--- R/clean_after_geocode.R
+++ R/clean_after_geocode.R
@@ -3,14 +3,20 @@ library(readr)
 library(janitor)
 source("R/col_types_wells.R")
 
-# this is the list of wells we already geocoded
-geocoded <- read_csv("github_data/wells_geocoded.csv", col_types = col_types_geocoded)
+library(RPostgres)
+con1 <- DBI::dbConnect(
+  RPostgres::Postgres(),
+  dbname = Sys.getenv("BCGOV_DB"),
+  host = Sys.getenv("BCGOV_HOST"),
+  user = Sys.getenv("BCGOV_USR"),
+  password=Sys.getenv("BCGOV_PWD")
+)
 
 # this is the list of well we geocoded today
 newly_geocoded <- read_csv("data/wells_geocoded.csv", col_types = col_types_geocoded)
 
-# update list of geocoded wells
-all_geocoded <- bind_rows(geocoded,newly_geocoded) %>%
-  arrange(well_tag_number)
-
-write_csv(all_geocoded, "data/wells_geocoded.csv")  
+if(nrow(newly_geocoded)> 0){
+  message("Appending newly geocoded  wells:", nrow(newly_geocoded, " rows"))
+  dbAppendTable(con1, "wells_geocoded", newly_geocoded)
+  
+} else{message("No new geocodedwells to append.")}
