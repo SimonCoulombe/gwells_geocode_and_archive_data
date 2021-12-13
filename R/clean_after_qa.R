c80a62a9624985fb101b907c96829bb6ca3a3e95
Simon Coulombe <simoncoulombe@gmail.com>
2021-12-13 14:37:42 -0500

    more debug

--- R/clean_after_qa.R
+++ R/clean_after_qa.R
@@ -14,6 +14,10 @@ con1 <- DBI::dbConnect(
   password=Sys.getenv("BCGOV_PWD")
 )
 
+old_qa <- dbGetQuery(con1, "select * from wells_qa limit 10")
+message("printing out QA")
+glimpse(old_qa)
+
 
 newly_qa <- read_csv("gwells_locationqa.csv", col_types = col_types_qa) %>%
   select(-one_of(c("Unnamed: 0"))) %>% 
@@ -22,8 +26,12 @@ newly_qa <- read_csv("gwells_locationqa.csv", col_types = col_types_qa) %>%
 
 
 if(nrow(newly_qa)> 0){
+  
+  message("printing new qa")
+  glimpse(newly_qa)
   message("Appending newly qa  wells:", nrow(newly_qa), " rows. well_tag_number=", paste(newly_qa$well_tag_number, collase = " "))
   dbAppendTable(con1, "wells_qa", newly_qa)
+  message("done")
   
 } else{message("No new qa wells to append.")}
 
